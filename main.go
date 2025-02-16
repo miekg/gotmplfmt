59540607f87ec44d4292e7cb68a0bb84e6ac3384
Miek Gieben <miek@miek.nl>
2025-02-16 11:57:56 +0100

    meer
    Signed-off-by: Miek Gieben <miek@miek.nl>

--- main.go
+++ main.go
@@ -14,41 +14,55 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	// Example template
-	tmpl := string(buf)
 
+	tmpl := string(buf)
 	treeSet := make(map[string]*parse.Tree)
-	t := parse.New("example")
+	t := parse.New("Zgotmplfmt")
 	t.Mode = parse.ParseComments | parse.SkipFuncCheck
 	if _, err = t.Parse(tmpl, "{{", "}}", treeSet); err != nil {
 		fmt.Println("Error parsing template:", err)
 		return
 	}
 	for n, ts := range treeSet {
-		println("TREE NAME", n)
+		fmt.Printf("{{define %q }}\n", n)
 		printAST(ts.Root, 0)
+		fmt.Println("{{end}}")
 	}
 }
 
-func printAST(node parse.Node, depth int) {
+func printAST(node parse.Node, depth int, elseif ...bool) {
+	fmt.Printf("**KEY %T***\n", node)
 	indent := strings.Repeat("  ", depth)
 	switch n := node.(type) {
 	case *parse.ActionNode:
-		fmt.Printf("%s action %s\n", indent, n.String())
+		fmt.Printf("%s", n.String())
 	case *parse.TextNode:
-		fmt.Printf("%s text %s %s\n", indent, n.String(), n.Text)
+		//format this html
+		fmt.Printf("%s", n.Text)
 	case *parse.StringNode:
-		fmt.Printf("%s string %s %s\n", indent, n.String(), n.Text)
+		fmt.Printf("%s", n.Quoted)
 	case *parse.IdentifierNode:
-		fmt.Printf("%s IdentifierNode %s %s\n", indent, n.String(), n.Ident)
+		fmt.Printf("%s", n.Ident)
 	case *parse.IfNode:
-		fmt.Printf("%sIfNode:%s\n", indent, n.String())
+		if len(elseif) > 0 {
+			fmt.Printf(" if ")
+		} else {
+			fmt.Printf("%s{{if ", indent)
+		}
 		printAST(n.Pipe, depth+1)
+		fmt.Println("}}")
 		printAST(n.List, depth+1)
 		if n.ElseList != nil {
-			fmt.Printf("%sElse:\n", indent)
-			printAST(n.ElseList, depth+1)
+			if _, ok := n.ElseList.Nodes[0].(*parse.IfNode); ok { // else if construct
+				fmt.Printf("%s{{else ", indent)
+				printAST(n.ElseList, depth+1, true)
+				fmt.Printf("}}\n")
+			} else {
+				fmt.Printf("%s{{else}}", indent)
+				printAST(n.ElseList, depth+1)
+			}
 		}
+		fmt.Printf("{{end}}")
 	case *parse.RangeNode:
 		fmt.Printf("%sRangeNode:\n", indent)
 		printAST(n.Pipe, depth+1)
@@ -58,27 +72,22 @@ func printAST(node parse.Node, depth int) {
 			printAST(n.ElseList, depth+1)
 		}
 	case *parse.ListNode:
-		fmt.Printf("%sListNode:\n", indent)
 		for _, child := range n.Nodes {
 			printAST(child, depth+1)
 		}
 	case *parse.PipeNode:
-		fmt.Printf("%sPipeNode:\n", indent)
 		for _, cmd := range n.Cmds {
 			printAST(cmd, depth+1)
 		}
 	case *parse.CommandNode:
-		fmt.Printf("%sCommandNode: %s\n", indent, n.String())
 		for _, arg := range n.Args {
 			printAST(arg, depth+1)
 		}
 	case *parse.FieldNode:
-		fmt.Printf("%s field %s", indent, n.Ident)
+		fmt.Printf(".%s", strings.Join(n.Ident, "."))
 	case *parse.VariableNode:
 		fmt.Printf("%s var %s", indent, n.Ident)
 	case *parse.TemplateNode:
-		fmt.Printf("%s %s", indent, n.String())
-	case *parse.CommentNode:
 		fmt.Printf("%s %s", indent, n.String())
 	default:
 		fmt.Printf("%sUnknown Node: %T\n", indent, n)
