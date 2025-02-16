4a7f1cf3035787924ce0f89b1ab32e671dd2156f
Miek Gieben <miek@miek.nl>
2025-02-16 11:23:14 +0100

    Getting there
    Signed-off-by: Miek Gieben <miek@miek.nl>

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/miekg/gohtmlfmt
+module github.com/miekg/gotmplfmt
 
 go 1.24.0
 
--- main.go
+++ main.go
@@ -20,8 +20,7 @@ func main() {
 	treeSet := make(map[string]*parse.Tree)
 	t := parse.New("example")
 	t.Mode = parse.ParseComments | parse.SkipFuncCheck
-	tree, err := t.Parse(tmpl, "{{", "}}", treeSet)
-	if err != nil {
+	if _, err = t.Parse(tmpl, "{{", "}}", treeSet); err != nil {
 		fmt.Println("Error parsing template:", err)
 		return
 	}
@@ -29,12 +28,59 @@ func main() {
 		println("TREE NAME", n)
 		printAST(ts.Root, 0)
 	}
-	println("PARSED TREE")
-
-	printAST(tree.Root, 0)
 }
 
 func printAST(node parse.Node, depth int) {
 	indent := strings.Repeat("  ", depth)
-	fmt.Printf("%s%s", indent, node.String())
+	switch n := node.(type) {
+	case *parse.ActionNode:
+		fmt.Printf("%s action %s\n", indent, n.String())
+	case *parse.TextNode:
+		fmt.Printf("%s text %s %s\n", indent, n.String(), n.Text)
+	case *parse.StringNode:
+		fmt.Printf("%s string %s %s\n", indent, n.String(), n.Text)
+	case *parse.IdentifierNode:
+		fmt.Printf("%s IdentifierNode %s %s\n", indent, n.String(), n.Ident)
+	case *parse.IfNode:
+		fmt.Printf("%sIfNode:%s\n", indent, n.String())
+		printAST(n.Pipe, depth+1)
+		printAST(n.List, depth+1)
+		if n.ElseList != nil {
+			fmt.Printf("%sElse:\n", indent)
+			printAST(n.ElseList, depth+1)
+		}
+	case *parse.RangeNode:
+		fmt.Printf("%sRangeNode:\n", indent)
+		printAST(n.Pipe, depth+1)
+		printAST(n.List, depth+1)
+		if n.ElseList != nil {
+			fmt.Printf("%sElse:\n", indent)
+			printAST(n.ElseList, depth+1)
+		}
+	case *parse.ListNode:
+		fmt.Printf("%sListNode:\n", indent)
+		for _, child := range n.Nodes {
+			printAST(child, depth+1)
+		}
+	case *parse.PipeNode:
+		fmt.Printf("%sPipeNode:\n", indent)
+		for _, cmd := range n.Cmds {
+			printAST(cmd, depth+1)
+		}
+	case *parse.CommandNode:
+		fmt.Printf("%sCommandNode: %s\n", indent, n.String())
+		for _, arg := range n.Args {
+			printAST(arg, depth+1)
+		}
+	case *parse.FieldNode:
+		fmt.Printf("%s field %s", indent, n.Ident)
+	case *parse.VariableNode:
+		fmt.Printf("%s var %s", indent, n.Ident)
+	case *parse.TemplateNode:
+		fmt.Printf("%s %s", indent, n.String())
+	case *parse.CommentNode:
+		fmt.Printf("%s %s", indent, n.String())
+	default:
+		fmt.Printf("%sUnknown Node: %T\n", indent, n)
+	}
 }
