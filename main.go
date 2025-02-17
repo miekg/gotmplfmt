d5287420384ab0a5516be1eff4f90d88306a58b0
Miek Gieben <miek@miek.nl>
2025-02-17 15:03:33 +0100

    looks promising
    Signed-off-by: Miek Gieben <miek@miek.nl>

--- main.go
+++ main.go
@@ -3,115 +3,15 @@ package main
 import (
 	"fmt"
 	"io"
-	"log"
 	"os"
-	"strings"
-	"text/template/parse"
-)
-
-const (
-	Failsafe = "ZgotmplfmtZ"
-	End      = "{{end}}"
 )
 
 func main() {
-	buf, err := io.ReadAll(os.Stdin)
-	if err != nil {
-		log.Fatal(err)
-	}
+	buf, _ := io.ReadAll(os.Stdin)
+	lexer := NewLexer(string(buf))
+	tokens := lexer.Lex()
 
-	w := New(os.Stdout)
-
-	treeSet, err := parseTree(buf)
-	if err != nil {
-		log.Fatalf("Failed to parse template: %s", err)
-	}
-	for n, ts := range treeSet {
-		// check ts.Root, might also be a block?
-		fmt.Printf("** %T --", ts.Root)
-		fmt.Printf("%d\n", len(ts.Root.Nodes))
-		fmt.Fprintf(w, "{{define %q}}\n***\n", n)
-		w.Pretty(ts.Root, 0)
-		fmt.Fprintf(w, "%s\n", End)
-	}
-}
-
-func parseTree(buf []byte) (map[string]*parse.Tree, error) {
-	treeSet := make(map[string]*parse.Tree)
-	t := parse.New(Failsafe)
-	t.Mode = parse.ParseComments | parse.SkipFuncCheck
-
-	_, err := t.Parse(string(buf), "{{", "}}", treeSet)
-	return treeSet, err
-}
-
-func (w *W) Pretty(node parse.Node, depth int, elseif ...bool) {
-	//	fmt.Fprintf(w, "**KEY %T***\n", node)
-	w.Indent(depth)
-	switch n := node.(type) {
-	case *parse.ActionNode:
-		fmt.Fprintf(w, "%s", n.String())
-	case *parse.TextNode:
-		w.Text(n.Text, depth)
-		w.Newline()
-	case *parse.StringNode:
-		fmt.Fprintf(w, "%s", n.Quoted)
-	case *parse.IdentifierNode:
-		fmt.Fprintf(w, "%s", n.Ident)
-	case *parse.IfNode:
-		if len(elseif) > 0 { // we are in a {{else if ...
-			fmt.Fprintf(w, "if ")
-		} else {
-			fmt.Fprint(w, "{{if ")
-		}
-		w.Pretty(n.Pipe, depth)
-
-		fmt.Fprintln(w, "}}")
-
-		w.Pretty(n.List, depth+1)
-		if n.ElseList != nil {
-			if _, ok := n.ElseList.Nodes[0].(*parse.IfNode); ok { // else if construct
-				fmt.Fprintf(w, "%s{{else ", indent)
-				w.Pretty(n.ElseList.Nodes[0], depth, true)
-				for _, child := range n.ElseList.Nodes[1:] {
-					w.Pretty(child, depth+1)
-				}
-				return
-			} else {
-				fmt.Fprintf(w, "%s{{else}}", indent)
-				w.Newline()
-				w.Pretty(n.ElseList, depth+1)
-			}
-		}
-		w.Indent(depth)
-		fmt.Fprintf(w, "%s\n", End)
-	case *parse.RangeNode:
-		fmt.Fprintf(w, "%sRangeNode:\n", indent)
-		w.Pretty(n.Pipe, depth+1)
-		w.Pretty(n.List, depth+1)
-		if n.ElseList != nil {
-			fmt.Fprintf(w, "%sElse:\n", indent)
-			w.Pretty(n.ElseList, depth+1)
-		}
-	case *parse.ListNode:
-		for _, child := range n.Nodes {
-			w.Pretty(child, depth+1)
-		}
-	case *parse.PipeNode:
-		for _, cmd := range n.Cmds {
-			w.Pretty(cmd, depth+1)
-		}
-	case *parse.CommandNode:
-		for _, arg := range n.Args {
-			w.Pretty(arg, depth+1)
-		}
-	case *parse.FieldNode:
-		fmt.Fprintf(w, ".%s", strings.Join(n.Ident, "."))
-	case *parse.VariableNode:
-		fmt.Fprintf(w, "%s var %s", indent, n.Ident)
-	case *parse.TemplateNode:
-		fmt.Fprintf(w, "%s %s", indent, n.String())
-	default:
-		fmt.Fprintf(w, "%sUnknown Node: %T\n", indent, n)
+	for _, token := range tokens {
+		fmt.Printf("Type: %v, Value: %q\n", token.Type, token.Value)
 	}
 }
