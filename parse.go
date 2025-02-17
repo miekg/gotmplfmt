8b5f5c926dd5226867759a6bfac02b6aaad90439
Miek Gieben <miek@miek.nl>
2025-02-17 19:15:23 +0100

    wip
    Signed-off-by: Miek Gieben <miek@miek.nl>

--- parse.go
+++ parse.go
@@ -6,40 +6,37 @@ import (
 
 // Node is the parse tree of a template.
 type Node struct {
-	Tokens []Token
+	Token  Token
 	List   []*Node
 	Parent *Node
 }
+
+func NewNode(parent *Node) *Node { return &Node{Parent: parent} }
 
 // Parse parses the tokens and adds them the list in n. It returns the token that are not yet consumed.
 func (n *Node) parse(tokens []Token) []Token {
 	if len(tokens) == 0 {
 		return nil
 	}
-	if tokens[0].Type == TokenText {
-		// add to the current node and continue with the rest
-		n.Tokens = append(n.Tokens, tokens[0])
-		return n.parse(tokens[1:])
-	}
 
-	// TokenTemplate, add them all as a list
 	switch tokens[0].Subtype {
 	case End:
-		// end ends the current list addition and we fall back to the parent
-		n.Parent.Tokens = append(n.Parent.Tokens, tokens[0])
+		// end ends the current list addition and we fall back to the parent, add the {{end}} to the parent List
+		// instead of on this level.
+		n1 := &Node{Token: tokens[0], Parent: n}
+		n.Parent.List = append(n.Parent.List, n1)
 		return n.Parent.parse(tokens[1:])
 
-	case Define:
-		n.Tokens = append(n.Tokens, tokens[0])
-		n1 := &Node{Parent: n}
+	case Define, If, ElseIf, Else:
+		n1 := &Node{Token: tokens[0], Parent: n}
 		n.List = append(n.List, n1)
 		return n1.parse(tokens[1:])
 
 	default:
-		// add to the current open node
-		n.Tokens = append(n.Tokens, tokens[0])
+		// also includes TokenText, by having a zero subtype
+		n1 := &Node{Token: tokens[0], Parent: n}
+		n.List = append(n.List, n1)
 		return n.parse(tokens[1:])
-
 	}
 }
 
@@ -48,13 +45,11 @@ func Parse(tokens []Token) error {
 		fmt.Printf("Type: %v, Subtype: %v, Value: %q\n", token.Type, token.Subtype, token.Value)
 	}
 
-	tree := &Node{}
+	tree := NewNode(nil)
 	tree.Parent = tree
 
 	tree.parse(tokens)
 
-	println("**AST**")
-	Tree(tree, 0)
 	println("**AST**")
 	Pretty(tree, 0)
 
@@ -63,18 +58,15 @@ func Parse(tokens []Token) error {
 
 func Pretty(n *Node, depth int) {
 
-	for _, t := range n.Tokens {
-		fmt.Printf("[%d] %s\n", depth, t.Value)
-	}
+	fmt.Printf("[%d] %s\n", depth, n.Token.Value)
 	for i := range n.List {
 		Pretty(n.List[i], depth+1)
 	}
 }
 
 func Tree(n *Node, depth int) {
-	fmt.Printf("[%d] %d tokens %d nodes\n", depth, len(n.Tokens), len(n.List))
+	fmt.Printf("[%d] token: %v %d nodes\n", depth, n.Token, len(n.List))
 	for i := range n.List {
 		Tree(n.List[i], depth+1)
 	}
-
 }
